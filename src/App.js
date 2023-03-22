5a7c6f879951292b1a3ca34ee22044c8a74d0a7b
KaylaneBiazoto <103209586+KaylaneBiazoto@users.noreply.github.com>
2023-03-22 13:41:27 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css'
+import Header from './Components/Header'
+import Meme from './Components/Meme'
+
+export default () => {
+  return (
+    <>
+      <Header />
+      <Meme />
+    </>
+  )
+}
